f4feca0990f219b9600ee419f145347aab372dcc
Thijs Jung <thijs.jung@trivago.com>
2020-08-31 14:33:33 +0200

    Delete old models and writers

--- src/models/music.py
+++
@@ -1,9 +0,0 @@
-from collections import namedtuple
-
-class Chord(namedtuple('Chord', ['name', 'addition', 'duration'])):
-    @property
-    def dict(self):
-        return dict(self._asdict())
-
-class Measure():
-    pass
--- src/models/song.py
+++
@@ -1,5 +0,0 @@
-class Song():
-    def __init__(self, structure, lyrics, harmony):
-        self.structure = structure
-        self.lyrics = lyrics
-        self.harmony = harmony
